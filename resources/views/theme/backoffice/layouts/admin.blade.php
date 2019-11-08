578ea11cfd15f54bc65b1ad1552dcbb21ae2575e
FranzEdel <franz.t77@gmail.com>
2019-11-08 01:21:12 -0400

    Revert "CRUD roles"
    This reverts commit 66cc3c6ff3b968084e5c0bc8584486d534bb8aa4.

--- resources/views/theme/backoffice/layouts/admin.blade.php
+++ resources/views/theme/backoffice/layouts/admin.blade.php
@@ -17,7 +17,6 @@
       <div class="wrapper">
         @include('theme.backoffice.layouts.includes.left-sidebar')
         <section id="content">
-          @include('theme.backoffice.layouts.includes.breadcrumbs')
           <div class="container">
             @yield('content')
           </div>
